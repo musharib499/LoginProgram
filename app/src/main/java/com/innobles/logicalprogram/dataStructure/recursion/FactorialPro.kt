85cd48b3a3602911caff6c3acb72d16104f70a34
Musharib <ali.musharib1@gmail.com>
2020-12-29 14:54:08 +0530

    all program push

---
+++ app/src/main/java/com/innobles/logicalprogram/dataStructure/recursion/FactorialPro.kt
@@ -0,0 +1,25 @@
+package com.innobles.logicalprogram.dataStructure.recursion
+
+/**
+ * Created by Musharib Ali on 28/12/20.
+ * I.S.T Pvt. Ltd
+ * musharib.ali@innobles.com
+ */
+fun main() {
+println(fact(4))
+for (i in 0..5){
+    println(fibonacci(i))
+}
+}
+
+fun fact(n:Int):Int{
+    if (n<=1) return 1
+    return n* fact(n-1)
+
+}
+
+fun fibonacci(n:Int):Int{
+    if (n == 0) return 0
+    if (n == 1 || n == 2) return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+}
