0fb960b2b3688671a8b6e9d3d7fc8a676f3b3547
Mushareb <apple@Musharibs-MacBook-Pro.local>
2023-10-10 19:15:01 +0530

    add cherry pick commit

--- app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
+++ app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
@@ -1,5 +1,7 @@
 package com.app.logicprogram.kotlindemo
 
+import java.math.BigDecimal
+
 fun main() {
     println(1 plus  3)
     println(2 multiple   3)
@@ -12,6 +14,8 @@ infix fun Int.plus(x: Int): Int { return this + x }
 
 infix fun Int.multiple(x: Int): Int { return this * x }
 infix fun Long.multiple(x: Long): Long { return this * x }
+infix fun BigDecimal.multiple(x: BigDecimal): BigDecimal { return this * x }
+infix fun Double.multiple(x: Double): Double { return this * x }
 infix fun String.plus(x: String): String { return this + x }
 infix fun List<Any>.plusItem(x:Any):List<Any> {
     return this.plus(x)
