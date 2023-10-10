122378e3ccb12b180481e2127c75f2fc51754842
APPLE <apple@Musharibs-MacBook-Pro.local>
2023-10-10 18:24:50 +0530

    Add long multiple infix function

--- app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
+++ app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
@@ -9,11 +9,15 @@ fun main() {
 }
 
 infix fun Int.plus(x: Int): Int { return this + x }
+
 infix fun Int.multiple(x: Int): Int { return this * x }
+infix fun Long.multiple(x: Long): Long { return this * x }
 infix fun String.plus(x: String): String { return this + x }
 infix fun List<Any>.plusItem(x:Any):List<Any> {
     return this.plus(x)
 }
+
+
 
 
 
