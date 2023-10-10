8c7c610ce14bbef997345e3ac5ef266e4b19d93a
Mushareb <apple@Musharibs-MacBook-Pro.local>
2023-10-10 21:28:50 +0530

    Remove commit 19920f7

--- app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
+++ app/src/main/java/com/app/logicprogram/kotlindemo/InfixFunction.kt
@@ -1,7 +1,5 @@
 package com.app.logicprogram.kotlindemo
 
-import java.math.BigDecimal
-
 fun main() {
     println(1 plus  3)
     println(2 multiple   3)
@@ -14,8 +12,6 @@ infix fun Int.plus(x: Int): Int { return this + x }
 
 infix fun Int.multiple(x: Int): Int { return this * x }
 infix fun Long.multiple(x: Long): Long { return this * x }
-infix fun BigDecimal.multiple(x: BigDecimal): BigDecimal { return this * x }
-infix fun Double.multiple(x: Double): Double { return this * x }
 infix fun String.plus(x: String): String { return this + x }
 infix fun List<Any>.plusItem(x:Any):List<Any> {
     return this.plus(x)
